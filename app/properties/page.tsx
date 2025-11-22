9e6d8f8f060497ec21509b998dfeda500344e678
MSUResult <adarshrajput9040@gmail.com>
2025-11-22 13:19:25 +0530

    first commit

---
+++ app/properties/page.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export function generateMetadata() {
+  return {
+    title: "Property Listing Site in Saharanpur",
+    description:
+      "Here you can list the property of Saharanpur for rent and sale.",
+  };
+}
+const Properties = () => {
+  return (
+    <main className=" h-screen w-full">
+      <section className="bg-blue-500 rounded-[25px] h-[80%] "></section>
+    </main>
+  );
+};
+
+export default Properties;
--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -1,8 +1,33 @@
+"use client";
+import { useRouter } from "next/navigation";
+
 export default function Navbar() {
+  const router = useRouter();
+  const items = [
+    { name: "About", href: "/about" },
+    { name: "Properties", href: "/properties" },
+    { name: "Agents", href: "/agents" },
+    { name: "Blogs", href: "/blogs" },
+  ];
   return (
+    <main className="flex justify-between py-7  font-bold">
+      <h1>SaharanpurProperty</h1>
 
-  )
-   <main className="">
+      <section className="flex gap-7 ">
+        {items.map((item, index) => (
+          <div
+            key={index}
+            className=" hover:text-gray-400 cursor-pointer"
+            onClick={() => router.push(item.href)}
+          >
+            {item.name}
+          </div>
+        ))}
+      </section>
 
-  </main>
+      <button className="bg-blue-500 rounded-[25px] text-white cursor-pointer p-3 px-4">
+        Contact Us
+      </button>
+    </main>
+  );
 }
